02a284dd1f3aef11c0ac8fabffc7a1a1bdda2633
Christian Liman <christianliman94@gmail.com>
2015-11-09 10:12:48 +0800

    Further improved

--- plot3.R
+++ plot3.R
@@ -14,16 +14,16 @@ household$DateTime <- paste(household$Date, household$Time)
 household$DateTime <- strptime(household$DateTime, "%Y-%m-%d %H:%M:%S")
 
 
-# Creating the plot
+# Creating the plot and saving to a png file
+
+png(file = "plot3.png", width = 480, height = 480)
 
 with(household, {
   plot(DateTime, Sub_metering_1, type = "l", ylab = "Energy sub metering", xlab = "")
   lines(DateTime, Sub_metering_2, col = "red")
   lines(DateTime, Sub_metering_3, col = "blue")
   })
-legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), bty = "n", cex = 0.75)
+legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), bty = "n")
 
-# Saving to a png file
 
-dev.copy(png, "plot3.png")
 dev.off()
--- plot4.R
+++ plot4.R
@@ -13,9 +13,11 @@ household$DateTime <- paste(household$Date, household$Time)
 household$DateTime <- strptime(household$DateTime, "%Y-%m-%d %H:%M:%S")
 household$Global_reactive_power_kw <- as.numeric(household$Global_reactive_power)/500
 
-# Creating the plot
+# Creating the plot and saving it to a png file
 
+png(file = "plot4.png", width = 480, height = 480)
 par(mfrow = c(2, 2))
+
 with(household, {
   plot(DateTime, Global_active_power_kw, type = "l", xlab = "", ylab = "Global Active Power")
   plot(DateTime, Voltage, type = "l", xlab = "datetime")
@@ -23,12 +25,9 @@ with(household, {
     plot(DateTime, Sub_metering_1, type = "l", ylab = "Energy sub metering", xlab = "")
     lines(DateTime, Sub_metering_2, col = "red")
     lines(DateTime, Sub_metering_3, col = "blue")
-    legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), bty = "n", cex = 0.75)
+    legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), bty = "n")
   })
   plot(DateTime, Global_reactive_power_kw, type = "l", xlab = "datetime")
 })
 
-# Saving to a png file
-
-dev.copy(png, "plot4.png")
 dev.off()
